ca46bb01934635775b1702dadba3548b98e2e593
jiin995 <gabrieleprevitera@gmail.com>
2019-06-08 16:10:34 +0200

    Completato

--- Model/sudoku_model.py
+++ Model/sudoku_model.py
@@ -1,7 +1,8 @@
 # applichiamo il pattern Singleton alla classe
 # per gestire un'unica griglia sudoku
+
 class SudokuGrid:
-    default_grid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+    default_grid = '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
     grid = ''
     digits = '123456789'
     delimiters = '0.'
@@ -22,10 +23,6 @@ class SudokuGrid:
     def set_grid(self, grid: str):
         self.grid = grid
 
-    # TODO INSERIRE VALIDAZIONE CON GUROBI
     def is_valid_grid(self):
         return len([char for char in self.grid if char in SudokuGrid.valid_chars]) == 81
 
-    # TODO CREARE SEEDS SIA SEMIVUOTIVUOTI CHE PIENI
-    # TODO QUELLI PIENI SERVONO PER GENERARE IL SUDOKU (PIENA -> SEMIVUOTA)
-    # TODO QUELLI SEMIVUOTI SERVONO PER CARICARE RANDOMICAMENTE IL SUDOKU INIZIALE
